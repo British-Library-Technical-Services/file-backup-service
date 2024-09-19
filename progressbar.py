ba44761e0128928d6ad84f4aeee9156868998e72
realgoodegg <tomisalive@gmail.com>
2024-09-19 13:55:03 +0100

    Added cycling_progress function

--- progressbar.py
+++ progressbar.py
@@ -1,4 +1,5 @@
 from rich import print
+import time
 
 
 def progress_bar(current, total, bar_length=20):
@@ -15,3 +16,18 @@ def progress_bar(current, total, bar_length=20):
         print(
             f"[bold green]Complete: [------------------->] 100 % [{current}/{total}][/bold green]"
         )
+
+def cycling_progress(mirror_is_running):
+    if mirror_is_running:
+        bar_length = 20
+        for i in range(bar_length):
+            cycle = "." * i
+            space = " " * int(bar_length - i)
+            print(f"[bold yellow]Processing[/bold yellow]: \[{cycle}{space}]" , end="\r") 
+            time.sleep(0.3)
+    else:
+        print("[bold green]Complete: \[....................][/bold green]")
+
+
+# mirror_is_running = True
+# cycling_progress(mirror_is_running)
